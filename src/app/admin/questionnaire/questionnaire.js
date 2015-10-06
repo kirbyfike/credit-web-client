7fa24b695d4cab42525adae311524c52ab8276e4
Kirby Fike <kfike@pnmac-939788.pnmac.com>
2015-10-06 13:50:17 -0700

    added nav for questionnaire

--- src/app/admin/questionnaire/questionnaire.js
+++ src/app/admin/questionnaire/questionnaire.js
@@ -5,14 +5,15 @@ angular.module( 'credit.admin.questionnaire', [
   $stateProvider
     .state( 'adminQuestionnaire', {
       url: '/admin/questionnaire',
+      abstract: true,
       views: {
       	"header": {
           controller: 'HeaderCtrl',
           templateUrl: 'app/shared/header/header.tpl.html'
         },
       	"nav": {
-          controller: 'NavCtrl',
-          templateUrl: 'app/shared/nav/nav.tpl.html'
+          controller: 'AdminQuestionnaireNavCtrl',
+          templateUrl: 'app/admin/questionnaire/questionnaire.nav.tpl.html'
         },
         "main": {
           controller: 'AdminQuestionnaireCtrl',
@@ -20,13 +21,58 @@ angular.module( 'credit.admin.questionnaire', [
         }
       },
       data:{ pageTitle: 'Login' }
-    });
+    })
+    .state( 'adminQuestionnaire.index', {
+      url: '',
+      controller: 'AdminQuestionnaireIndexCtrl',
+      templateUrl: 'app/admin/questionnaire/questionnaire.index.tpl.html',
+      data:{ pageTitle: 'Model' }
+    })
+    .state( 'adminQuestionnaire.categories', {
+      url: '',
+      controller: 'AdminQuestionnaireCategoriesCtrl',
+      templateUrl: 'app/admin/questionnaire/questionnaire.categories.tpl.html',
+      data:{ pageTitle: 'Model' }
+    })
+    .state( 'adminQuestionnaire.questions', {
+      url: '',
+      controller: 'AdminQuestionnaireQuestionsCtrl',
+      templateUrl: 'app/admin/questionnaire/questionnaire.questions.tpl.html',
+      data:{ pageTitle: 'Model' }
+    })
+    ;
 })
 
-/**
- * And of course we define a controller for our route.
- */
+// .factory('Questionnaire', function ($resource, $rootScope)  {
+//   var Questionnaire = $resource("questionnaire.json", {id:'@id'}, {
+//     update: { 
+//         method: 'PUT', 
+//         params: { id: '@id' }
+//     },
+//     remove: {method:'DELETE'}
+//   });
+
+//   return Questionnaire;
+// })
+
+
 .controller( 'AdminQuestionnaireCtrl', function AdminQuestionnaireCtrl($scope, $state) {
-  console.log("hello");
+  
+})
+
+.controller( 'AdminQuestionnaireIndexCtrl', function AdminQuestionnaireIndexCtrl($scope, $state, Questionnaire) {
+  
+})
+
+.controller( 'AdminQuestionnaireNavCtrl', function AdminQuestionnaireNavCtrl($scope, $state) {
+  console.log("index");
+})
+
+.controller( 'AdminQuestionnaireCategoriesCtrl', function AdminQuestionnaireCategoriesCtrl($scope, $state) {
+  console.log("index");
+})
+
+.controller( 'AdminQuestionnaireQuestionsCtrl', function AdminQuestionnaireQuestionsCtrl($scope, $state) {
+  console.log("index");
 })
 ;
